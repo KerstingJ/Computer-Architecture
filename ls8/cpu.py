d4172849b949baeb8c02ede0778a10fc069a7090
Josh <Kersting.Josh@Gmail.com>
2019-08-22 18:15:23 -0400

    added Call and ret

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,8 @@ import sys
 DEBUGGING = False
 
 # so I don't have to comment and uncomment prints
+
+
 def debug(message):
     if DEBUGGING:
         print(message)
@@ -79,89 +81,155 @@ class CPU:
         self.RH = address
 
     def ADD(self):
+        debug(f"ADD R{self.ram_read(self.PC+1)}, R{self.ram_read(self.PC+2)}")
+
+        self.reg[self.ram_read(
+            self.PC+1)] = self.reg[self.ram_read(self.PC+1)] + self.reg[self.ram_read(self.PC+2)]
+        self.PC += 3
         pass
 
     def AND(self):
+        debug("calling ADD")
+        self.PC += 1
         pass
 
     def CALL(self):
+        debug(f"CALL R{self.ram_read(self.PC + 1)}")
+        """
+        Call a subroutine from an address stored in register
+        address of the next instruction is pushed onto the stack
+        set pc to address stored in given register
+        jump to that location and run the first command
+        then move pc to loc popped from stack 
+
+        CALL r0
+
+        """
+
+        # push PC+2 onto the stack to retrieve later
+        self.reg[self.SP] -= 1
+        self.ram_write(self.reg[self.SP], self.PC + 2)
+        # debug(f"pushed {self.PC + 2} to stack")
+
+        # get subroutine from given register
+        self.PC = self.reg[self.ram_read(self.PC + 1)]
+
         pass
 
     def CMP(self):
+        debug("calling CMP")
+        self.PC += 1
         pass
 
     def DEC(self):
+        debug("calling DEC")
+        self.PC += 1
         pass
 
     def DIV(self):
+        debug("calling DIV")
+        self.PC += 1
         pass
 
     def HLT(self):
+        debug("calling HLT")
+        self.PC += 1
         sys.exit()
 
     def INC(self):
+        debug("calling INC")
+        self.PC += 1
         pass
-        
+
     def INT(self):
+        debug("calling INT")
+        self.PC += 1
         pass
 
     def IRET(self):
+        debug("calling IRET")
+        self.PC += 1
         pass
 
     def JEQ(self):
+        debug("calling JEQ")
+        self.PC += 1
         pass
 
     def JGE(self):
+        debug("calling JGE")
+        self.PC += 1
         pass
 
     def JGT(self):
+        debug("calling JGT")
+        self.PC += 1
         pass
 
     def JLE(self):
+        debug("calling JLE")
+        self.PC += 1
         pass
 
     def JLT(self):
+        debug("calling JLT")
+        self.PC += 1
         pass
 
     def JMP(self):
+        debug("calling JMP")
+        self.PC += 1
         pass
 
     def JNE(self):
+        debug("calling JNE")
+        self.PC += 1
         pass
 
     def LD(self):
+        debug("calling LD")
+        self.PC += 1
         pass
 
     def LDI(self):
+        debug(f"LDI R{self.ram_read(self.PC + 1)}, {self.ram_read(self.PC + 2)}")
         reg = self.ram_read(self.PC + 1)
         self.reg[reg] = self.ram_read(self.PC + 2)
-        # store second arg in register
-        debug(
-            f"set R{self.ram_read(self.PC + 1)} to {self.ram_read(self.PC + 2)}")
-        self.PC += 2
+        self.PC += 3
 
     def MOD(self):
+        debug("calling MOD")
+        self.PC += 1
         pass
 
     def MUL(self):
+        debug(
+            f"MUL R{self.ram_read(self.PC + 1)}, R{self.ram_read(self.PC + 2)}")
         # MUL
         # multiple register A by register b
         # store new value in register A
         reg_a = self.reg[self.ram_read(self.PC + 1)]
         reg_b = self.reg[self.ram_read(self.PC + 2)]
         self.reg[self.ram_read(self.PC + 1)] = reg_a * reg_b
-        self.PC += 2
+        self.PC += 3
 
     def NOP(self):
+        debug("calling NOP")
+        self.PC += 1
         pass
 
     def NOT(self):
+        debug("calling NOT")
+        self.PC += 1
         pass
 
     def OR(self):
+        debug("calling OR")
+        self.PC += 1
         pass
 
     def POP(self):
+        debug(f"POP R{self.ram_read(self.PC + 1)}")
         # get register id from ram
         reg = self.ram_read(self.PC + 1)
         # set registers value to last value on stack
@@ -169,17 +237,21 @@ class CPU:
         # increment stack
         self.reg[self.SP] += 1
         # increment PC to pass memory spaces ready by program
-        self.PC += 1
+        self.PC += 2
 
     def PRA(self):
+        debug("calling PRA")
+        self.PC += 1
         pass
 
     def PRN(self):
+        debug("calling PRN")
         debug(f"printing register {self.ram_read(self.PC + 1)}")
         print(self.reg[self.ram_read(self.PC + 1)])
-        self.PC += 1
+        self.PC += 2
 
     def PUSH(self):
+        debug("calling PUSH")
         # decrement stack pointer by 1
         self.reg[self.SP] -= 1
 
@@ -194,24 +266,37 @@ class CPU:
         # write to address of SP value of reg
         self.ram_write(self.reg[self.SP], self.reg[reg])
 
-        self.PC += 1
+        self.PC += 2
 
     def RET(self):
-        pass
+        debug("calling RET")
+        # set PC to location popped from stack
+        self.PC = self.ram_read(self.reg[self.SP])
+        self.reg[self.SP] += 1
 
     def SHL(self):
+        debug("calling SHL")
+        self.PC += 1
         pass
 
     def SHR(self):
+        debug("calling SHR")
+        self.PC += 1
         pass
 
     def ST(self):
+        debug("calling ST")
+        self.PC += 1
         pass
 
     def SUB(self):
+        debug("calling SUB")
+        self.PC += 1
         pass
 
     def XOR(self):
+        debug("calling XOR")
+        self.PC += 1
         pass
 
     def alu(self, op, reg_a, reg_b):
@@ -242,8 +327,10 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+        sys.exit(1)
 
     def run(self):
+        debug("calling run")
         """Run the CPU."""
         self.PC = 0  # point to beginning of RAM
         self.reg[self.SP] = -1  # point to end of RAM
@@ -257,5 +344,3 @@ class CPU:
             else:
                 print(f"could not recognize command: {self.IR}")
                 self.trace()
-
-            self.PC += 1
